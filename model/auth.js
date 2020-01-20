978d4871ac0ac9ef13930de0e74d831750de667a
adtbo <adtmustika@gmail.com>
2020-01-20 20:45:39 +0800

    adding some hash encryption. going to test it tommorow

---
+++ model/auth.js
@@ -0,0 +1,31 @@
+const db = require("../config/db");
+
+const Auth = function(auth) {
+  this.email = auth.email;
+  this.passcode = auth.passcode;
+};
+
+Auth.authenticate = newAuth => {
+  var query =
+    "SELECT * FROM Users WHERE username = ? AND password = ? AND Users.EndDateTime IS NULL";
+
+  var username = request.body.username;
+  var passcode = request.body.passcode;
+  if (username && password) {
+    bcrypt.compare(passcode, hash).then(function(res) {
+      db.query(query, [username, hash], function(error, results, fields) {
+        if (results.length > 0 && res) {
+        //   request.session.loggedin = true;
+        //   request.session.username = username;
+        //   response.redirect("/home");
+        } else {
+        //   response.send("Incorrect Username and/or Password!");
+        }
+        // response.end();
+      });
+    });
+  } else {
+    // response.send("Please enter Username and Password!");
+    // response.end();
+  }
+};
--- model/user.model.js
+++ model/user.model.js
@@ -1,103 +1,110 @@
-const db = require('../config/db');
+const db = require("../config/db");
+const bcrypt = required('bcrypt');
+const saltRounds = 10;
 
-const User = function (user) {
-    this.userId = user.userId;
-    this.name = user.name;
-    this.email = user.email;
-    this.passcode = user.passcode;
+const User = function(user) {
+  this.userId = user.userId;
+  this.name = user.name;
+  this.email = user.email;
+  this.passcode = user.passcode;
 };
 
-
 User.create = newUser => {
-    var query = 'INSERT INTO Users (userd, Name, Email, Passcode) VALUES (?);';
-    query =
-      query +
-      'UPDATE Users SET Users.userd = LAST_INSERT_ID() Users.id = LAST_INSERT_ID()';
+  var query = "INSERT INTO Users (userd, Name, Email, Passcode) VALUES (?);";
+  query =
+    query +
+    "UPDATE Users SET Users.userd = LAST_INSERT_ID() Users.id = LAST_INSERT_ID()";
+
+  bcrypt.hash(newUser.passcode, saltRounds, function(err, hash) {
+    newUser.passcode = hash;
     db.query(query, newUser, (err, res) => {
       if (err) {
         queryResult.status = 400;
-        queryResult.message = 'Error occured while creating User due to ' + err;
+        queryResult.message = "Error occured while creating User due to " + err;
         queryResult.data = [];
       } else {
         queryResult.status = 200;
-        queryResult.message = 'Create User success';
+        queryResult.message = "Create User success";
         queryResult.data = res;
       }
-  
+
       return queryResult;
     });
-  };
-  
-  User.update = (id, User) => {
-    var dataById = this.getById(id);
-    if (dataById.status === 200) {
-      var query =
-        'UPDATE Users SET Users.EndDateTime = now() WHERE Users.userd = ? AND Users.EndDateTime IS NULL; INSERT INTO Users (userd, Name, Email, Passcode) VALUES (?);';
-  
+  });
+};
+
+User.update = (id, User) => {
+  var dataById = this.getById(id);
+  if (dataById.status === 200) {
+    var query =
+      "UPDATE Users SET Users.EndDateTime = now() WHERE Users.userd = ? AND Users.EndDateTime IS NULL; INSERT INTO Users (userd, Name, Email, Passcode) VALUES (?);";
+
+    bcrypt.hash(newUser.passcode, saltRounds, function(err, hash) {
+      newUser.passcode = hash;
       db.query(query, [id, User], function(error, rows, fields) {
         if (error) {
           queryResult.status = 400;
-          queryResult.message = 'Update User failed';
+          queryResult.message = "Update User failed";
         } else {
           queryResult.status = 200;
-          queryResult.message = 'Update User success';
+          queryResult.message = "Update User success";
         }
       });
-    }
-    return queryResult;
-  };
-  
-  User.getAll = () => {
-    db.query('SELECT * FROM Users', (err, row, fields) => {
-      if (err) {
-        queryResult.data = [];
-        queryResult.message = 'Fetch Users failed';
-        queryResult.status = 400;
-      }
-      queryResult.data = row;
-      queryResult.status = 200;
-      queryResult.message = 'Fetch Users success';
     });
-  
-    return queryResult;
-  };
-  
-  User.getById = function(id) {
+  }
+  return queryResult;
+};
+
+User.getAll = () => {
+  db.query("SELECT * FROM Users", (err, row, fields) => {
+    if (err) {
+      queryResult.data = [];
+      queryResult.message = "Fetch Users failed";
+      queryResult.status = 400;
+    }
+    queryResult.data = row;
+    queryResult.status = 200;
+    queryResult.message = "Fetch Users success";
+  });
+
+  return queryResult;
+};
+
+User.getById = function(id) {
+  var query =
+    "SELECT Users.* FROM Users WHERE Users.userd = ? AND Users.EndDateTime IS NULL";
+  db.query(query, id, function(error, rows, field) {
+    if (error) {
+      (queryResult.status = 400), (queryResult.data = []);
+    } else {
+      (queryResult.status = 200), (queryResult.data = rows);
+    }
+  });
+  return queryResult;
+};
+
+User.deleteUser = function(id) {
+  var dataFetch = this.getById(id);
+  if (dataFetch.status === 200) {
     var query =
-      'SELECT Users.* FROM Users WHERE Users.userd = ? AND Users.EndDateTime IS NULL';
+      "UPDATE Users SET Users.EndDateTime = now() WHERE Users.userd = ? AND Users.EndDateTime IS NULL";
     db.query(query, id, function(error, rows, field) {
       if (error) {
-        (queryResult.status = 400), (queryResult.data = []);
+        queryResult.status = 401;
+        queryResult.message = "Delete User error due to " + error;
+        queryResult.data = [];
       } else {
-        (queryResult.status = 200), (queryResult.data = rows);
+        (queryResult.data = []),
+          (queryResult.message = "Delete User success"),
+          (queryResult.status = 200);
       }
     });
-    return queryResult;
-  };
-  
-  User.deleteUser = function(id) {
-    var dataFetch = this.getById(id);
-    if (dataFetch.status === 200) {
-      var query =
-        'UPDATE Users SET Users.EndDateTime = now() WHERE Users.userd = ? AND Users.EndDateTime IS NULL';
-      db.query(query, id, function(error, rows, field) {
-        if (error) {
-          queryResult.status = 401;
-          queryResult.message = 'Delete User error due to ' + error;
-          queryResult.data = [];
-        } else {
-          (queryResult.data = []),
-            (queryResult.message = 'Delete User success'),
-            (queryResult.status = 200);
-        }
-      });
-    } else {
-      queryResult.status = 404;
-      queryResult.message = 'User Id not found';
-      queryResult.data = [];
-    }
-    return queryResult;
-  };
-  
-  module.exports = User;
-  
+  } else {
+    queryResult.status = 404;
+    queryResult.message = "User Id not found";
+    queryResult.data = [];
+  }
+  return queryResult;
+};
+
+module.exports = User;
