b26f761226dbd04745d71f8635a3ca64dd9ecc1a
adtbo <adtmustika@gmail.com>
2020-03-14 14:27:17 +0700

    route user role addition

--- routes/userRoles.js
+++ routes/userRoles.js
@@ -3,26 +3,26 @@ const UserRoleController = require('../controller/UserRoleController');
 
 module.exports = function(app) {
     var userRoleCont = new UserRoleController();
-    app.route('/roles').get((req, res)=>
+    app.route('/userroles').get((req, res)=>
         userRoleCont
             .findAll()
             .then(data => res.send(data))
             .catch(err => res.send(err))
     );
-    app.route('/roles/:id').get((req, res)=> {
+    app.route('/userroles/:id').get((req, res)=> {
         const id = req.params.id;
         userRoleCont
             .findById(id)
             .then(data => res.send(data))
             .catch(err => res.status(400).send('Error occured due to ' + err))
     });
-    app.route('/roles').post((req, res)=>
+    app.route('/userroles').post((req, res)=>
         userRoleCont
             .createUserRole(req)
             .then(data => res.send(data))
             .catch(err => res.send(err))
     );
-    app.route('/roles/:id').delete((req, res)=> {
+    app.route('/userroles/:id').delete((req, res)=> {
         const id = req.params.id;
         RoleCont
           .deleteUserRole(id)
