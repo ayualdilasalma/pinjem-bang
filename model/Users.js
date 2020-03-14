f5adf6d663dbad1aa999b9338b4ec7a259aa8854
ayu aldila salma <ayu.aldilas@yahoo.com>
2020-03-14 15:02:20 +0700

    add user controller

--- model/Users.js
+++ model/Users.js
@@ -1,25 +1,35 @@
 const sequelize = require('sequelize');
 const db = require('../config/dbORM');
 
-const Users = db.define('Users', {
-  UserId: {
-    type: sequelize.DataTypes.INTEGER
+const Users = db.define(
+  'Users',
+  {
+    UserId: {
+      type: sequelize.DataTypes.INTEGER
+    },
+    Name: {
+      type: sequelize.DataTypes.STRING
+    },
+    Email: {
+      type: sequelize.DataTypes.STRING
+    },
+    Password: {
+      type: sequelize.DataTypes.STRING
+    },
+    StartDateTime: {
+      type: sequelize.DataTypes.DATE
+    },
+    EndDateTime: {
+      type: sequelize.DataTypes.DATE
+    }
   },
-  Name: {
-    type: sequelize.DataTypes.STRING
-  },
-  Email: {
-    type: sequelize.DataTypes.STRING
-  },
-  Password: {
-    type: sequelize.DataTypes.STRING
-  },
-  StartDateTime: {
-    type: sequelize.DataTypes.DATE
-  },
-  EndDateTime: {
-    type: sequelize.DataTypes.DATE
+  {
+    // enable timestamps
+    timestamps: true,
+    createdAt: false,
+    StartDateTime: 'updateTimestamp',
+    updatedAt: false
   }
-});
+);
 
 module.exports = Users;
